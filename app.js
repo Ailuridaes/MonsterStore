4755d83b7901c6a5e8f92b5b9da057ea4e608c08
Ailuridaes <ailuridaes@gmail.com>
2016-03-24 19:58:18 -0700

    refactoring

--- app.js
+++ app.js
@@ -1,86 +1,8 @@
 (function (){
-  var app = angular.module('store', []);
+  var app = angular.module('store', ['store-monsters']);
 
   app.controller('StoreController', function(){
     this.monsters = monsters;
-  });
-
-  app.controller('ReviewController', function() {
-    this.review = {};
-
-    this.addReview = function(monster) {
-      monster.reviews.push(this.review);
-      this.review = {};
-    };
-  });
-
-  app.directive('productPanels', function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'product-panels.html',
-      controller: function(){
-        this.tab = 1;
-
-        this.selectTab = function(setTab) {
-          this.tab = setTab;
-        };
-
-        this.isSelected = function(checkTab) {
-          return this.tab === checkTab;
-        };
-      },
-      controllerAs: 'panel'
-    };
-  });
-
-  app.directive('gallery', function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'gallery.html',
-      controller: function(){
-        this.current = 0;
-
-        this.setCurrent = function(setCurrent) {
-          this.current = setCurrent ? setCurrent : 0;
-        };
-      },
-      controllerAs: 'gallery'
-    };
-  });
-
-  app.directive('monsterTitle', function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'monster-title.html'
-    };
-  });
-
-  app.directive('monsterDescription', function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'monster-description.html'
-    };
-  });
-
-  app.directive("monsterSpecs", function() {
-    return {
-      restrict: 'A',
-      templateUrl: 'monster-specs.html'
-    };
-  });
-
-  app.directive("review", function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'review.html'
-    };
-  });
-
-  app.directive("reviewForm", function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'review-form.html'
-    };
   });
 
   var monsters = [
---
+++ monsters.js
@@ -0,0 +1,81 @@
+(function (){
+  var app = angular.module('store-monsters', []);
+  app.directive('productPanels', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'product-panels.html',
+      controller: function(){
+        this.tab = 1;
+
+        this.selectTab = function(setTab) {
+          this.tab = setTab;
+        };
+
+        this.isSelected = function(checkTab) {
+          return this.tab === checkTab;
+        };
+      },
+      controllerAs: 'panel'
+    };
+  });
+
+  app.directive('gallery', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'gallery.html',
+      controller: function(){
+        this.current = 0;
+
+        this.setCurrent = function(setCurrent) {
+          this.current = setCurrent ? setCurrent : 0;
+        };
+      },
+      controllerAs: 'gallery'
+    };
+  });
+
+  app.directive('monsterTitle', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'monster-title.html'
+    };
+  });
+
+  app.directive('monsterDescription', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'monster-description.html'
+    };
+  });
+
+  app.directive("monsterSpecs", function() {
+    return {
+      restrict: 'A',
+      templateUrl: 'monster-specs.html'
+    };
+  });
+
+  app.directive("review", function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'review.html'
+    };
+  });
+
+  app.directive("reviewForm", function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'review-form.html',
+      controller: function() {
+        this.review = {};
+
+        this.addReview = function(monster) {
+        monster.reviews.push(this.review);
+        this.review = {};
+        };
+      },
+      controllerAs: 'reviewCtrl'
+    };
+  });
+
+})();
